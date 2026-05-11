a1e1a5ae0fbbc04d9b8a88be613d5306824740f9
benotar <zhmur.ivan@gmail.com>
2026-05-11 17:31:17 +0300

    feat(catalog): add get artist by id feature

---
+++ src/Services/Catalog/Eventify.Catalog.Application/Artists/Queries/GetArtistByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using Eventify.Catalog.Application.Artists.Responses;
+using Eventify.SharedKernel.Application.CQRS;
+
+namespace Eventify.Catalog.Application.Artists.Queries;
+
+public sealed record GetArtistByIdQuery(Guid Id) : IQuery<ArtistResponse>;
---
+++ src/Services/Catalog/Eventify.Catalog.Application/Artists/Queries/GetArtistByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using ErrorOr;
+using Eventify.Catalog.Application.Artists.Responses;
+using Eventify.Catalog.Application.Interfaces;
+using Eventify.Catalog.Domain.Artists.ValueObjects;
+using Eventify.SharedKernel.Application.CQRS;
+using Eventify.SharedKernel.Extensions;
+
+namespace Eventify.Catalog.Application.Artists.Queries;
+
+public sealed class GetArtistByIdQueryHandler : IQueryHandler<GetArtistByIdQuery, ArtistResponse>
+{
+    private readonly IArtistRepository _artistRepository;
+
+    public GetArtistByIdQueryHandler(IArtistRepository artistRepository)
+    {
+        _artistRepository = artistRepository;
+    }
+
+    public async Task<ErrorOr<ArtistResponse>> Handle(GetArtistByIdQuery query, CancellationToken ct)
+    {
+        if (query.Id.IsEmpty)
+        {
+            return Error.Validation(description: "Artist id is invalid.");
+        }
+
+        var artistId = ArtistId.Of(query.Id);
+
+        var artist = await _artistRepository.GetByIdAsync(artistId, ct);
+
+        if (artist is null)
+        {
+            return Error.NotFound(description: "Artist not found.");
+        }
+
+        return artist.ToResponse();
+    }
+}
